fafae5e2839174c9fcf0dd582034d535d83d03e3
Angel Garcia <angel.rene.garcia13@gmail.com>
2014-09-15 11:17:10 -0400

    NavbarLinkHelper Added

---
+++ SimpleAuthMember/Helpers/NavbarLinkHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleAuthMember.Helpers
+{
+    public static class NavbarLinkHelper
+    {
+        public static MvcHtmlString NavbarLink(this HtmlHelper helper, string name, string actionName, string controllerName)
+        {
+            string currentControllerName = (string)helper.ViewContext.RouteData.Values["controller"];
+            string currentActionName = (string)helper.ViewContext.RouteData.Values["action"];
+            
+            string isActiveClass = "";
+            if(currentControllerName.Equals(controllerName, StringComparison.CurrentCultureIgnoreCase) && currentActionName.Equals(actionName, StringComparison.CurrentCultureIgnoreCase)){
+                isActiveClass = " class=\"active\"";
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendFormat("<li {0}>", isActiveClass);
+            var url = new UrlHelper(HttpContext.Current.Request.RequestContext);
+            sb.AppendFormat("<a href=\"{0}\">{1}</a>", url.Action(actionName, controllerName), name);
+            sb.Append("</li>");
+            return new MvcHtmlString(sb.ToString());
+}
+    }
+}
