2c679e8e0d4471409c82e452e943a262e9845adb
Angel Garcia <angel.rene.garcia13@gmail.com>
2014-09-16 14:06:25 -0400

    Backup DB Created
    At dbo/Tables/

--- dbo/Tables/Users.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Users] (
-    [ID]          INT            IDENTITY (1, 1) NOT NULL,
-    [UserName]    NVARCHAR (100) NULL,
-    [DisplayName] NVARCHAR (150) NULL,
-    [Country]     VARCHAR (150)  NULL,
-    PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
